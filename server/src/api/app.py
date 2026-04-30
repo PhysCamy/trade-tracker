86731c15fceb8898f2d29fe43188f3f87f57df91
Cameron Matchett <cmatchett@live.co.uk>
2026-04-30 09:58:16 +0100

    Setting up frontend config

--- server/src/api/app.py
+++ server/src/api/app.py
@@ -18,7 +18,9 @@ origins = [
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins
+    allow_origins=origins,
+    allow_methods=['*'],
+    allow_headers=['*']
 )
 
 # Portfolio CRUD endpoints
