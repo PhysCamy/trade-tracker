0c51803c3c5560141272f0834df1704920aaab10
Cameron Matchett <cmatchett@live.co.uk>
2026-05-29 17:09:24 +0100

    Adding grid display for Portfolios

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,27 @@ import React from 'react';
 import './App.css';
 import Sidebar from './components/Sidebar';
 import Home from './components/Home';
+import Switch from '@mui/material/Switch';
+import Portfolios from './components/Portfolios';
 
 const App = () => {
+  let component;
+
+  switch (window.location.pathname) {
+    case "/":
+      component = <Home />;
+      break;
+    case "/portfolios":
+      component = <Portfolios />;
+      break;
+    default:
+      component = <Home />
+  };
+
   return (
-    <main className="grid gap-4 grid-cols-[200px,_1fr]">
+    <main className="grid grid-cols-[200px,_1fr]">
       <Sidebar />
-      <Home />
+      {component}
     </main>
   );
 };
--- client/src/components/Portfolios.jsx
+++ client/src/components/Portfolios.jsx
@@ -1,19 +1,25 @@
 import React, { useEffect, useState} from 'react';
 import api from '../api.js';
 import AddPortfolioForm from './AddPortfolioForm.jsx';
+import { AllCommunityModule } from 'ag-grid-community';
+import { AgGridProvider, AgGridReact } from 'ag-grid-react';
+import { themeBalham } from 'ag-grid-community';
 
 const Portfolios = () => {
     const [portfolios, setPortfolios] = useState([]);
-
+    const [colDefs, setColDefs] = useState([
+        { field: "id", headerName: "Portfolio ID", flex: 1 },
+        { field: "name", headerName: "Name", flex: 1 },
+        { field: "baseCcy", headerName: "Base Currency", flex: 1 }
+    ]);
     const fetchPortfolios = async () => {
         try {
             const response = await api.get('/portfolios');
-            setPortfolios(response.data)
+            setPortfolios(response.data.map((portfolio, index) => ({ id: portfolio.id, name: portfolio.name, baseCcy: portfolio.baseCcy ?? "ZZZ" })));
         } catch (error) {
             console.error("Error fetching portfolios.")
         }
     }
-
     const addPortfolio = async (id, name, baseCcy) => {
     try {
       await api.post('/portfolios/', { id: id, name: name, base_ccy: baseCcy });
@@ -28,13 +34,17 @@ const Portfolios = () => {
   }, []);
 
   return (
-    <div>
-      <h2>Portfolios</h2>
-      <ul>
-        {portfolios.map((portfolio, index) => (
-          <li key={index}>{portfolio.name}</li>
-        ))}
-      </ul>
+    <div className="bg-white pb-4 h-[200vh]">
+      <AgGridProvider modules={[AllCommunityModule]}>
+        <div style={{ height: 500 }}>
+            <AgGridReact
+                theme={themeBalham}
+                rowData={portfolios}
+                columnDefs={colDefs}
+                domLayout='autoHeight'
+            />
+        </div>
+      </AgGridProvider>
       <AddPortfolioForm addPortfolio={addPortfolio} />
     </div>
   );
